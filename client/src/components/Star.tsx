fab9a1c7165cc89e0f3d9549fcbce14c75ed5c92
Jeremy Kolassa <jdkolassa@gmail.com>
2022-06-04 21:09:08 -0400

    Reworked how I'm approaching the spectral types, added some documentation, and destructured the newly added props

--- client/src/components/Star.tsx
+++ client/src/components/Star.tsx
@@ -1,31 +1,22 @@
 import React from 'react';
 import { CCard, CCardBody, CCardSubtitle, CCardTitle, CListGroup, CListGroupItem } from '@coreui/react';
 
-import { LuminosityClasses, SpectralClasses, IStar } from '../models/IStar';
+import { IStar } from '../models/IStar';
 
 
 const Star = (props: IStar) => {
 
-    const {name, HIP, distance, hue, subclass, luminosity} = props;
+    const {name, otherNames, GJ, HD, HIP, distance, hue, luminosity, spectralType} = props;
 
-    function assembleColorCode(hue: SpectralClasses, subclass: number) {
-      // Trying to calculate with "5.5" classes is going to be hella-annoying
-      // So let's make sure we only use the subclass BEFORE the decimal
-      let subClean = String(subclass.toFixed(0));
-      return hue.concat(subClean);
-    }
-
-    function assembleSpect(hue: SpectralClasses, subclass: number, luminosity: LuminosityClasses) {
-        // First cast the values to strings
-        let hueString = String(hue);
-        let subcString = String(subclass);
-        let lumosString = String(luminosity);
-        // Then, let's concatenate them into a proper spectral classification
-        return hueString.concat(subcString, lumosString);
-    }
-
-    const Spect = assembleSpect(hue, subclass, luminosity);
-    const ColorCode = assembleColorCode(hue, subclass);
+    /**
+     * * Stars can have many names: a proper "name", catalogue numbers, and alternate names from history and different cultures. Proper and other names take priority, followd by Gliese, HD, and then HIP numbers.
+     * @param {string} [name] - proper name of star, if exists
+     * @param {string[]} [otherNames] - other names the star has been called
+     * @param {number} [GJ] - Gliese catalogue number, only nearby stars
+     * @param {number} [HD] - Henry Draper catalogue number
+     * @param {number} [HIP] - Hipparcos catalogue number
+     * @returns {string | string[]} - The star name(s) 
+     */
 
     const nameDisplay = () => {
       if(name === undefined && HIP !== undefined) {
@@ -44,19 +35,22 @@ const Star = (props: IStar) => {
 
     // TODO: Figure out how to turn the spectral class into an actual color for the card border
 
+    /**
+     * @returns {Star}
+     */
 
   return (
     <CCard className="starCard" id={name}>
-        <CCardBody className={`drawBox ${hue} ${subclass} ${luminosity}`}>
-          <div className={`star ${ColorCode}`}>
+        <CCardBody className={`drawBox `}>
+          <div className={`star ${hue} ${luminosity}`}>
 
           </div>
         </CCardBody>
         <CCardBody>
           {nameDisplay()}
           <CListGroup flush>
-            <CListGroupItem>Spectral Type: {Spect}</CListGroupItem>
-            <CListGroupItem>Distance: {distance}</CListGroupItem>
+            <CListGroupItem>Spectral Type: {spectralType}</CListGroupItem>
+            <CListGroupItem>Distance: {distance} ly</CListGroupItem>
           </CListGroup>
         </CCardBody>
         

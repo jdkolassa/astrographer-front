8baa75c0b89168b3a273efbf46dba04a2ec08f4d
Jeremy Kolassa <jdkolassa@gmail.com>
2022-06-04 21:06:48 -0400

    I realized stars would have many more names & catalogue numbers, so I fleshed out the interface to account for them.

--- client/src/models/IStar.ts
+++ client/src/models/IStar.ts
@@ -1,11 +1,14 @@
 export interface IStar {
-    _id: string,
-    name?: string,
-    HIP?: number,
-    distance: number,
-    hue: SpectralClasses, // * must be a capital letter
-    subclass: number,
-    luminosity: LuminosityClasses,
+    _id: string | number, // this will be the id in the MongoDB store
+    name?: string, // Not all stars will have names, but they should if they don't have a HIP number
+    otherNames?: string[], // any other names the star has
+    GJ?: number, // Gliese Catalogue number, used for nearby stars
+    HD?: number, // Henry Draper Catalogue number
+    HIP?: number, // Hipparcos Catalogue number, used for many stars
+    distance: number, // In lightyears from Earth
+    hue: [SpectralClasses, number], // We'll use the first two parts to determine the color
+    luminosity: LuminosityClasses, // We'll use this for determining the star size
+    spectralType: string, // we're probably going to want the full spectralType for ease of use
 }
 
 /**

a9a3bce39caf3b80215a0cff75394245a3a77255
Jeremy Kolassa <jdkolassa@gmail.com>
2022-06-04 21:11:36 -0400

    Just writing out thoughts about importing all this CSV data into Mongo via Node/Mongoose

---
+++ server/db/import.ts
@@ -0,0 +1,8 @@
+/**
+ * This file will hold the logic for importing stars into the database
+ * 
+ */
+
+// TODO: Make sure to split the spectral type into two, with the first letter and number going in as hue and the roman numeral at the end as luminosity class
+
+// NOTE: I'll need a Node library that can parse large CSV files as that's what the original data will be in
